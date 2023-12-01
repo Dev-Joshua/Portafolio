4d8c9a9d86ce707972c55b20442e7ba040932f0f
Joshua Daniel <56142921+Dev-Joshua@users.noreply.github.com>
2023-12-01 15:40:06 -0500

    Update index.js

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,14 +1,12 @@
 const form = document.getElementById("form");
 const buttonMailto = document.getElementById("send-info");
-//Escucho el evento submit de mi formulario
+
 form.addEventListener("submit", handleSubmit);
 
 function handleSubmit(event) {
   event.preventDefault();
-  //Capturo la informacion del formulario. this hace referencia al elemento html $form
   const form = new FormData(this);
-  // console.log(form.get('name'))
-  // Debo llenar la informacion, modificare el atributo href, el segundo argumento es lo que ira nuevo()
+
   buttonMailto.setAttribute(
     "href",
     `mailto:joshua_dup@hotmail.com?subject=${form.get("name")} ${form.get(
